70c3482f7435b6b7617d5f3225065f476d3d3075
Zen-Rofiqy <zenr.prog@gmail.com>
2024-10-22 13:34:12 +0700

    emmet berhasil cuy

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,11 @@ import "./header.css";
 const header = () => {
   return (
     <header className='header'>
-        
+        <nav className="nav container">
+          <a href="index.html" className="nav_logo">Smith</a>
+
+          div.
+        </nav>
     </header>
   )
 }
