ee3009687a0c4f71cfe34453e3946dd711bebf75
Zen-Rofiqy <zenr.prog@gmail.com>
2024-10-29 13:55:26 +0700

    Add socmed to home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "./home.css";
+import Social from './Social';
 
 const Home = () => {
   return (
     <section className="home sectiom" id="home">
         <div className="home__container container grid">
-            <div className="home__container grid"></div>
+            <div className="home__container grid">
+              <Social />
+
+              <div className="home__img"></div>
+
+              <Data />
+            </div>
         </div>
     </section>
   )
