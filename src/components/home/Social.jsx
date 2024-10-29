88bb7f0f4a73ed52e2470bd46c2413aa9d80a748
Zen-Rofiqy <zenr.prog@gmail.com>
2024-10-29 13:53:34 +0700

    add socmed

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/function_runing/" className="home__social-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+        </a>
+        
+        <a href="https://rpubs.com/ZenR_Prog/" className="home__social-icon" target="_blank">
+            <i class="uil uil-dribbble"></i>
+        </a>
+        
+        <a href="https://github.com/Zen-Rofiqy" className="home__social-icon" target="_blank">
+            <i class="uil uil-github-alt"></i>
+        </a>
+        
+    </div>
+  )
+}
+
+export default Social
