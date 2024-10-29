692157c0a64c77a9985522431c5767999e8f3775
Zen-Rofiqy <zenr.prog@gmail.com>
2024-10-29 14:45:22 +0700

    fixing typo

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/function_runing/" className="home__social-icon" target="_blank">
-            <i class="uil uil-instagram"></i>
-        </a>
-        
-        <a href="https://rpubs.com/ZenR_Prog/" className="home__social-icon" target="_blank">
-            <i class="uil uil-dribbble"></i>
-        </a>
-        
-        <a href="https://github.com/Zen-Rofiqy" className="home__social-icon" target="_blank">
-            <i class="uil uil-github-alt"></i>
-        </a>
-        
-    </div>
-  )
-}
+      <a
+        href="https://www.instagram.com/function_runing/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
 
-export default Social
+      <a
+        href="https://www.linkedin.com/in/angga-fathan-rofiqy/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/Zen-Rofiqy"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
