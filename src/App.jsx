069f658a83ff3d09973c9d24cbbf53994954670a
Zen-Rofiqy <zenr.prog@gmail.com>
2024-10-30 21:49:35 +0700

    add about

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import Header from './components/header/Header';
-import Home from './components/home/Home';
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 const App = () => {
   return (
     <>
-    <Header />
+      <Header />
 
-    <main className='main'>
-      <Home />
-    </main>
+      <main className="main">
+        <Home />
+        <About />
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
