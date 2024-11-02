9a5985006d3ed0cd5159bfd60620039630404b65
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-02 13:10:25 +0700

    all open & close button

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -92,14 +92,23 @@ const Services = () => {
             </h3>
           </div>
 
-          <span className="services__button">
+          <span onClick={() => toggleTab(2)} className="services__button">
             View More
             <i className="uil uil-arrow-right services__button-icon"></i>
           </span>
 
-          <div className="services__modal">
+          <div
+            className={
+              toggleState === 2
+                ? "services__modal active-modal"
+                : "services__modal"
+            }
+          >
             <div className="services__modal-content">
-              <i className="uil uil-times services__modal-close"></i>
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services__modal-close"
+              ></i>
 
               <h3 className="services__modal-title">Ui/Ux Designer</h3>
               <p className="services__modal-description">
@@ -153,14 +162,23 @@ const Services = () => {
             </h3>
           </div>
 
-          <span className="services__button">
+          <span onClick={() => toggleTab(3)} className="services__button">
             View More
             <i className="uil uil-arrow-right services__button-icon"></i>
           </span>
 
-          <div className="services__modal">
+          <div
+            className={
+              toggleState === 3
+                ? "services__modal active-modal"
+                : "services__modal"
+            }
+          >
             <div className="services__modal-content">
-              <i className="uil uil-times services__modal-close"></i>
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services__modal-close"
+              ></i>
 
               <h3 className="services__modal-title">Visual Designer</h3>
               <p className="services__modal-description">
