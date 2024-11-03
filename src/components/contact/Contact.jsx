7b2eb61e49c789befc1ad908b85fd4140e143780
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-03 21:08:51 +0700

    contact info

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,64 @@ import React from "react";
 const Contact = () => {
   return (
     <section className="contact section" id="contact">
-      <h2 className="section__title">My clients say</h2>
-      <span className="section__subtitle">Testimonial</span>
+      <h2 className="section__title">Get in touch</h2>
+      <span className="section__subtitle">Contact Me</span>
+
+      <div className="contact__container container grid">
+        <div className="contact__content">
+          <h3 className="contact__title">Talk to me</h3>
+
+          <div className="contact__info">
+            <div className="contact__card">
+              <i className="bx bx contact__card-icon"></i>
+
+              <h3 className="contact__card-title">Email</h3>
+              <span className="contact__card-data">
+                fathan.rofiqy@gmail.com
+              </span>
+
+              <a
+                href="mailto:fathan.rofiqy@gmail.com.com"
+                className="contact__button"
+              >
+                Write me
+                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+              </a>
+            </div>
+
+            <div className="contact__card">
+              <i className="bx bx contact__card-icon"></i>
+
+              <h3 className="contact__card-title">Whatsapp</h3>
+              <span className="contact__card-data">+62 812-1091-7432</span>
+
+              <a href="https://wa.me/6281210917432" className="contact__button">
+                Write me
+                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+              </a>
+            </div>
+
+            <div className="contact__card">
+              <i className="bx bx contact__card-icon"></i>
+
+              <h3 className="contact__card-title">Instagram</h3>
+              <span className="contact__card-data">@function_runing</span>
+
+              <a
+                href="https://www.instagram.com/function_runing/"
+                className="contact__button"
+              >
+                Write me
+                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="contact__content">
+          <h3 className="contact__title">Write me your project</h3>
+        </div>
+      </div>
     </section>
   );
 };
