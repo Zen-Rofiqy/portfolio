0dde0a6c4fd5a64aca52ab9e20717616fd59e2b9
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-03 16:30:38 +0700

    typo

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -10,15 +10,13 @@ const Qualification = () => {
       <div className="qualification__container container">
         <div className="qualification__tabs">
           <div className="qualification__button qualification__active button--flex">
-            <i className="uil uil-graduation-cap qualification__icon">
-              Education
-            </i>
+            <i className="uil uil-graduation-cap qualification__icon"></i>
+            Education
           </div>
 
           <div className="qualification__button button--flex">
-            <i className="uil uil-briefcase-alt qualification__icon">
-              Experience
-            </i>
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
+            Experience
           </div>
         </div>
 
