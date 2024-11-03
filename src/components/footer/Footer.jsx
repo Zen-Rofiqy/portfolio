42f86fb9d2d39bbc19b0da447abcf59eed3c1491
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-03 23:20:24 +0700

    add social

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,6 +5,56 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__container container">
         <h1 className="footer__title">Zen Rofiqy</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a href="#testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://www.instagram.com/function_runing/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-instagram"></i>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/angga-fathan-rofiqy/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-linkedin"></i>
+          </a>
+
+          <a
+            href="https://github.com/Zen-Rofiqy"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-github"></i>
+          </a>
+        </div>
+
+        <span className="footer__copy">
+          &#169; Crypticalcoder. All rigths reserved
+        </span>
       </div>
     </footer>
   );
