9753c04ee2d721e05de2874f3902ab91be57b6fb
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-03 22:30:10 +0700

    styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -82,7 +82,7 @@ const Contact = () => {
               />
             </div>
 
-            <div className="contact__form-div">
+            <div className="contact__form-div contact__form-area">
               <label className="contact__form-tag">Project</label>
               <textarea
                 name="project"
