d41149c12df360d41796b5e0b41897a09f721a73
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-03 15:44:38 +0700

    edu & exp

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -4,8 +4,24 @@ import "./qualification.css";
 const Qualification = () => {
   return (
     <section className="qualification section">
-      <h2 className="section__title">Services</h2>
-      <span className="section__subtitle">What i offer</span>
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My personel journey</span>
+
+      <div className="qualification__container cointainer">
+        <div className="qualification__tabs">
+          <div className="qualification__button qualification__active button--flex">
+            <i className="uil uil-graduation-cap qualification__icon">
+              Education
+            </i>
+          </div>
+
+          <div className="qualification__button button--flex">
+            <i className="uil uil-briefcase-alt qualification__icon">
+              Experience
+            </i>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
