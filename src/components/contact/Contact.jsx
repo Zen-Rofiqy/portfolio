2bc1b82cd420da89b83a9bf6023f062192651436
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-03 20:45:58 +0700

    add contact

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section className="contact section" id="contact">
+      <h2 className="section__title">My clients say</h2>
+      <span className="section__subtitle">Testimonial</span>
+    </section>
+  );
+};
+
+export default Contact;
