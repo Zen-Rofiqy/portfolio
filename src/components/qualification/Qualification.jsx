91bd21925c1f9ea9b83c9d6f8e6edb0f48b7f08c
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-03 16:17:52 +0700

    add exp

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -23,7 +23,7 @@ const Qualification = () => {
         </div>
 
         <div className="qualification__sections">
-          <div className="qualification__content">
+          <div className="qualification__content qualification__content-active">
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">
@@ -90,6 +90,82 @@ const Qualification = () => {
                 </span>
                 <div className="qualification__calender">
                   <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="qualification__content">
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Data Analyst</h3>
+                <span className="qualification__subtitle">
+                  PT.ASDP Indonesia Ferry (Persero)
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> Sep 2024 - Des 2024
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Machine Learning Cohort
+                </h3>
+                <span className="qualification__subtitle">Bangkit Academy</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> Feb 2024 - Jul 2024
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Secretary</h3>
+                <span className="qualification__subtitle">
+                  Chess Unity of Agriculture IPB
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> Feb 2023 - Feb 2024
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">
+                  Graphic Designer and Data Analyst
+                </h3>
+                <span className="qualification__subtitle">
+                  Big Data Agrotourism PKM-RSH IPB University
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> Jan 2023 - Feb 2024
                 </div>
               </div>
             </div>
