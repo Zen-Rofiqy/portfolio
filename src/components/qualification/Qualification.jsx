e8d659277cc0b5ab2ad0d0319d56a2b7400cf8b6
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-03 16:07:13 +0700

    add edu

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -15,10 +15,84 @@ const Qualification = () => {
             </i>
           </div>
 
-          <div className="qualification__button button--flex">
+          <div className="qaualification__button button--flex">
             <i className="uil uil-briefcase-alt qualification__icon">
               Experience
             </i>
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div className="qualification__content">
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Statistics & Data Science
+                </h3>
+                <span className="qualification__subtitle">IPB University</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> Aug 2021 - Aug 2025
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Machine Learning Cohort
+                </h3>
+                <span className="qualification__subtitle">Bangkit Academy</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> Feb 2024 - Jul 2024
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Natural Science</h3>
+                <span className="qualification__subtitle">SMAN 1 Cibadak</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> Jan 2018 - Apr 2021
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Nah</h3>
+                <span className="qualification__subtitle">
+                  Institut Pertanian Bogor
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
