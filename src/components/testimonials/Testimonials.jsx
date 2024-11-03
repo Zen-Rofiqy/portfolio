c9deb9a826fa8319003991b3e9ecc1d8f4fb88ae
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-03 17:25:18 +0700

    add jsx

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <section className="testimonial container section">
+      <h2 className="section__title">My clients say</h2>
+      <span className="section__subtitle">Testimonial</span>
+    </section>
+  );
+};
+
+export default Testimonials;
