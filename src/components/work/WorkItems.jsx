1860338727990fe5848492515c659680904946fa
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-04 08:43:45 +0700

    add arrow

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -5,6 +5,8 @@ const WorkItems = ({ item }) => {
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="" className="work__img" />
       <h3 className="work__title">{item.title}</h3>
+      <a href="#" className="work__button"></a>
+      Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
     </div>
   );
 };
