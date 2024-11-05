8dceeea03538bc1fab90b3aa3857d28c99c2523b
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-05 11:25:45 +0700

    info

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -14,14 +14,14 @@ const Info = () => {
         <i class="bx bx-briefcase-alt about__icon"></i>
 
         <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">10+ Projects</span>
+        <span className="about__subtitle">20+ Projects</span>
       </div>
 
       <div className="about__box">
         <i class="bx bx-support about__icon"></i>
 
         <h3 className="about__title">Support</h3>
-        <span className="about__subtitle">Online 24/7</span>
+        <span className="about__subtitle">Online 9 to 5</span>
       </div>
     </div>
   );
