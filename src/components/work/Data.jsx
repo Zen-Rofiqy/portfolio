244ac39022b3d0dc17be8363584f7c223b4ccec0
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-07 05:23:37 +0700

    update

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -3,16 +3,16 @@ import Work2 from "../../assets/work_2.jpg";
 import Work3 from "../../assets/work_3.jpg";
 import Work4 from "../../assets/work_4.jpg";
 import Work5 from "../../assets/work_5.jpg";
-import Work1 from "../../assets/work_6.jpg";
-import Work2 from "../../assets/work_7.jpg";
-import Work3 from "../../assets/work_8.jpg";
-import Work4 from "../../assets/work_9.jpg";
-import Work5 from "../../assets/work_10.jpg";
-import Work1 from "../../assets/work_11.jpg";
-import Work2 from "../../assets/work_12.jpg";
-import Work3 from "../../assets/work_13.jpg";
-import Work4 from "../../assets/work_14.jpg";
-import Work5 from "../../assets/work_15.jpg";
+import Work6 from "../../assets/work_6.jpg";
+import Work7 from "../../assets/work_7.jpg";
+import Work8 from "../../assets/work_8.jpg";
+import Work9 from "../../assets/work_9.jpg";
+import Work10 from "../../assets/work_10.jpg";
+import Work11 from "../../assets/work_11.jpg";
+import Work12 from "../../assets/work_12.jpg";
+import Work13 from "../../assets/work_13.jpg";
+import Work14 from "../../assets/work_14.jpg";
+import Work15 from "../../assets/work_15.jpg";
 
 export const projectsData = [
   {
@@ -47,61 +47,61 @@ export const projectsData = [
   },
   {
     id: 6,
-    image: Work1,
+    image: Work6,
     title: "Surveyor - IPM Bandung",
     category: "statistics",
   },
   {
     id: 7,
-    image: Work2,
+    image: Work7,
     title: "App movil",
     category: "app",
   },
   {
     id: 8,
-    image: Work3,
+    image: Work8,
     title: "Brand design",
     category: "design",
   },
   {
     id: 9,
-    image: Work4,
+    image: Work9,
     title: "App movil",
     category: "app",
   },
   {
     id: 10,
-    image: Work5,
+    image: Work10,
     title: "Web design",
     category: "web",
   },
   {
     id: 11,
-    image: Work1,
+    image: Work11,
     title: "Surveyor - IPM Bandung",
     category: "statistics",
   },
   {
     id: 12,
-    image: Work2,
+    image: Work12,
     title: "App movil",
     category: "app",
   },
   {
     id: 13,
-    image: Work3,
+    image: Work13,
     title: "Brand design",
     category: "design",
   },
   {
     id: 14,
-    image: Work4,
+    image: Work14,
     title: "App movil",
     category: "app",
   },
   {
     id: 15,
-    image: Work5,
+    image: Work15,
     title: "Web design",
     category: "web",
   },
