0dcd4162679fb3cb269cf83b680a4c58448949fa
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-07 05:21:56 +0700

    update work data

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,8 +1,18 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
+import Work1 from "../../assets/work_1.jpg";
+import Work2 from "../../assets/work_2.jpg";
+import Work3 from "../../assets/work_3.jpg";
+import Work4 from "../../assets/work_4.jpg";
+import Work5 from "../../assets/work_5.jpg";
+import Work1 from "../../assets/work_6.jpg";
+import Work2 from "../../assets/work_7.jpg";
+import Work3 from "../../assets/work_8.jpg";
+import Work4 from "../../assets/work_9.jpg";
+import Work5 from "../../assets/work_10.jpg";
+import Work1 from "../../assets/work_11.jpg";
+import Work2 from "../../assets/work_12.jpg";
+import Work3 from "../../assets/work_13.jpg";
+import Work4 from "../../assets/work_14.jpg";
+import Work5 from "../../assets/work_15.jpg";
 
 export const projectsData = [
   {
@@ -31,6 +41,66 @@ export const projectsData = [
   },
   {
     id: 5,
+    image: Work5,
+    title: "Web design",
+    category: "web",
+  },
+  {
+    id: 6,
+    image: Work1,
+    title: "Surveyor - IPM Bandung",
+    category: "statistics",
+  },
+  {
+    id: 7,
+    image: Work2,
+    title: "App movil",
+    category: "app",
+  },
+  {
+    id: 8,
+    image: Work3,
+    title: "Brand design",
+    category: "design",
+  },
+  {
+    id: 9,
+    image: Work4,
+    title: "App movil",
+    category: "app",
+  },
+  {
+    id: 10,
+    image: Work5,
+    title: "Web design",
+    category: "web",
+  },
+  {
+    id: 11,
+    image: Work1,
+    title: "Surveyor - IPM Bandung",
+    category: "statistics",
+  },
+  {
+    id: 12,
+    image: Work2,
+    title: "App movil",
+    category: "app",
+  },
+  {
+    id: 13,
+    image: Work3,
+    title: "Brand design",
+    category: "design",
+  },
+  {
+    id: 14,
+    image: Work4,
+    title: "App movil",
+    category: "app",
+  },
+  {
+    id: 15,
     image: Work5,
     title: "Web design",
     category: "web",
