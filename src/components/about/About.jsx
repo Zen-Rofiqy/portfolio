c9287b9ec88518639633cdf9f08605bb79874ff1
Zen-Rofiqy <zenr.prog@gmail.com>
2024-11-06 14:48:39 +0700

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpeg";
-import CV from "../../assets/My-Cv.pdf";
+import CV from "../../assets/CV_Angga Fathan Rofiqy.pdf";
 import Info from "./Info";
 
 const About = () => {
